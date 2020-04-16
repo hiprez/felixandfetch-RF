498ba2587e185a0f80f2147be7530b00a90ff027
Robby Fairclough <robby.fairclough@gmail.com>
2020-04-16 14:37:18 -0700

    bootstrap

--- src/components/Type.js
+++ src/components/Type.js
@@ -8,28 +8,26 @@ class Type extends Component {
   render() {
     return (
       <div className="Type">
-        <div className="row">
-          <div
-            className={ 'col-6 ' + ( this.props.type === 'one' ? 'active' : '' ) }
-            onClick={ ( e ) => { this.onClick( 'one' ); } }>
-            <i class="fas fa-check-circle"></i>
-            <div>
-              <div className="icon-container">
-                <i className="fas fa-shopping-cart"></i>
-              </div>
-              <div>One Time Purchase</div>
+        <div
+          className={ this.props.type === 'one' ? 'active' : '' }
+          onClick={ ( e ) => { this.onClick( 'one' ); } }>
+          <i class="fas fa-check-circle"></i>
+          <div>
+            <div className="icon-container">
+              <i className="fas fa-shopping-cart"></i>
             </div>
+            <div>One Time Purchase</div>
           </div>
-          <div
-            className={ 'col-6 ' + ( this.props.type === 'sub' ? 'active' : '' ) }
-            onClick={ ( e ) => { this.onClick( 'sub' ); } }>
-            <i class="fas fa-check-circle"></i>
-            <div>
-              <div className="icon-container">
-                <i className="fas fa-calendar-plus"></i>
-              </div>
-              <div>Subscribe & Save *</div>
+        </div>
+        <div
+          className={ this.props.type === 'sub' ? 'active' : '' }
+          onClick={ ( e ) => { this.onClick( 'sub' ); } }>
+          <i class="fas fa-check-circle"></i>
+          <div>
+            <div className="icon-container">
+              <i className="fas fa-calendar-plus"></i>
             </div>
+            <div>Subscribe & Save *</div>
           </div>
         </div>
       </div>
